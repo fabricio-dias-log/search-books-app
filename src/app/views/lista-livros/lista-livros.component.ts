919459ad8804989f90a9c011ddfb96ef1f0dddbd
Fabricio Dias <fabricio270909@gmail.com>
2024-04-04 21:00:52 -0300

    Using switchMap and async pipe to optmize the search

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, switchMap, tap } from 'rxjs';
 import { Item } from 'src/app/models/item.interface';
 import { LivroVolumeInfo } from 'src/app/models/livro-volume-info';
 import { Livro } from 'src/app/models/livro.interface';
@@ -10,36 +11,19 @@ import { LivroService } from 'src/app/services/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[] = [];
-  campoBusca: string = '';
-  subscription: Subscription;
-  livro: Livro;
+  campoBusca = new FormControl();
 
   constructor(private livroService: LivroService) { }
 
-  buscarLivros() {
-    this.subscription = this.livroService.buscarLivros(this.campoBusca).subscribe({
-      next: (response) => {
-       this.listaLivros = this.livrosResultadoParaLivros(response);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-      complete: () => {
-        console.log('Requisição completa');
-      }
-    });
-
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    switchMap((valorDigitado)=> this.livroService.buscarLivros(valorDigitado)),
+    map((items: Item[]) => this.livrosResultadoParaLivros(items))
+  );
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => new LivroVolumeInfo(item));
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 }
 
