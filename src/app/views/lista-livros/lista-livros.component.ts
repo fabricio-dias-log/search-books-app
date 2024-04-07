5675d7a21ac1d42cd800b0a68079b7fde4a185a3
Fabricio Dias <fabricio270909@gmail.com>
2024-04-07 00:07:46 -0300

    Handling number of results

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { catchError, debounceTime, EMPTY, filter, map, switchMap, tap, throwError } from 'rxjs';
+import { catchError, debounceTime, EMPTY, filter, map, of, switchMap, tap, throwError } from 'rxjs';
 import { Item } from 'src/app/models/item.interface';
 import { LivroVolumeInfo } from 'src/app/models/livro-volume-info';
 import { Livro } from 'src/app/models/livro.interface';
+import { LivrosResultado } from 'src/app/models/livrosResultado.interface';
+import { VolumeInfo } from 'src/app/models/volumeInfo.interface';
 import { LivroService } from 'src/app/services/livro.service';
 
 const PAUSA = 300;
@@ -16,19 +18,21 @@ export class ListaLivrosComponent {
 
   campoBusca = new FormControl();
   mensagemErro = '';
+  livrosResultado: LivrosResultado;
+  listaLivros: LivroVolumeInfo[];
 
   constructor(private livroService: LivroService) { }
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     debounceTime(PAUSA),
-    filter(valorDigitado => valorDigitado.length > 3),
+    filter(valorDigitado => valorDigitado.length >= 3),
     switchMap((valorDigitado)=> this.livroService.buscarLivros(valorDigitado)),
-    map((items: Item[]) => this.livrosResultadoParaLivros(items)),
+    map(response => this.livrosResultado = response),
+    map(response => response.items ?? []),
+    map(items => this.listaLivros =  this.livrosResultadoParaLivros(items)),
     catchError(() => {
       this.mensagemErro = 'Erro ao buscar livros, recarregue a aplicação e tente novamente.';
       return EMPTY;
-      // console.error(error);
-      // return throwError(error => new Error(this.mensagemErro = 'Erro ao buscar livros'));
     })
   );
 
